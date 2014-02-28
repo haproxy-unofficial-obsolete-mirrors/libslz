b68027389ac0ddebd218e9631aa005f3139d3bf1
Willy Tarreau <w@1wt.eu>
2014-02-28 11:55:21 +0100

    exp: move the hashed pointer to 64 bits and stuff the data in it.
    We now store 64 bit values. The lowest part is the data, and the
    highest part is the offset. That allows us to avoid a memory access,
    because once the hashed entry is retrieved, it is already possible
    to check the data.
    
    The only problem is that it requires that the hash table is cleared
    upon each access. And intermediary solution could consist in just
    using this test as a hint to avoid the memory access as often as
    possible.
    
    Note that data are now checked 32 bit at a time. It happens that
    not handling 3-byte matches anymore slightly improves the amount of
    matching references, which is contrary to expectations.

--- src/exp-lm.c
+++ src/exp-lm.c
@@ -9,13 +9,20 @@ char *buffer;
 int bufsize = 16384;
 int buflen;
 
-#define HASH_BITS 16
+#define HASH_BITS 15
+
+/* stats */
+unsigned int lit = 0, ref = 0;
 
 /* format:
- * bit0..23 = last position in buffer of similar content
+ * bit0..31  = word
+ * bit32..63 = last position in buffer of similar content
  */
-uint32_t refs[1 << HASH_BITS];
+uint64_t refs[1 << HASH_BITS];
 
+/* This hash provides good average results on HTML contents, and is among the
+ * few which provide almost optimal results on various different pages.
+ */
 static inline uint32_t hash(uint32_t a)
 {
 	return ((a << 19) + (a << 6) - a) >> (32 - HASH_BITS);
@@ -29,8 +36,7 @@ static inline int memmatch(const char *a, const char *b, int max)
 	return len;
 }
 
-unsigned int /*totin = 0, */lit = 0, ref = 0;
-/* does 206 MB/s on non-compressible data, 286 MB/s on HTML. */
+/* does 290 MB/s on non-compressible data, 330 MB/s on HTML. */
 void encode(char *in, int ilen)
 {
 	int rem = ilen;
@@ -38,6 +44,7 @@ void encode(char *in, int ilen)
 	uint32_t word = 0;
 	int mlen;
 	uint32_t h, last;
+	uint64_t ent;
 
 	//printf("len = %d\n", ilen);
 	while (rem >= 2) {
@@ -50,26 +57,23 @@ void encode(char *in, int ilen)
 		__builtin_prefetch(refs + h);
 		pos++;
 		rem--;
-		last = refs[h];
-		refs[h] = pos - 1;
+		ent = refs[h];
+		last = ent >> 32;
+		refs[h] = (((uint64_t)(pos - 1)) << 32) + word;
 
-		if (last < pos - 1 && in[last] == (char)word) {
+		if (last < pos - 1 &&
+		    (uint32_t)ent == word) {
 			/* found a matching entry */
-			mlen = memmatch(in + pos, in + last + 1, rem);
-			if (mlen < 3)
-				goto tooshort;
-
+			mlen = 4 + memmatch(in + pos + 4, in + last + 5, rem);
 			//printf("found [%d]:0x%06x == [%d=@-%d] %d bytes\n",
 			//       pos - 1, word,
 			//       last, pos - 1 - last,
 			//       mlen + 1);
 			pos += mlen;
 			rem -= mlen;
-			//word = 0;
 			ref += mlen;
 		}
 		else {
-		tooshort:
 			//printf("litteral [%d]:%08x\n", pos - 1, word);
 			lit++;
 		}
